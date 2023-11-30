f5c6beb278acbe96f589490b9be0f39359ee62bf
sifisKoen <sifis.k.i@gmail.com>
2023-11-30 14:22:40 +0100

    Complete the distro_icon

--- trueline.sh
+++ trueline.sh
@@ -446,7 +446,7 @@ _trueline_cmd_duration_segment() {
 }
 
 _trueline_distro_icon_segment() {
-    case "$( grep ^ID= /etc/os-release )" in
+    case "$(grep ^ID= /etc/os-release)" in
         *ubuntu*) distro_icon=" " ;;
         *fedora*) distro_icon=" " ;;
         *debian*) distro_icon=" " ;;
